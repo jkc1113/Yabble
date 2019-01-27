409dddd830061ecdc897add723bc69c45f115a72
Kanuganti, Sravya [COM S] <sravyak@iastate.edu>
2019-01-27 14:35:43 -0600

    Adding front end code

---
+++ FrontEnd/Yabble/app/src/main/java/com/android/yabble/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.android.yabble;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#082567")));
+    }
+}
